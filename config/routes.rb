32a23992c81dfddd156d1a2bcc8513b002b3c30f
doanminhsang2003 <wuangsa29@gmail.com>
2025-01-07 16:29:00 +0700

    udpad

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,17 @@ Rails.application.routes.draw do
   get "/products", to: "products#index"
   get "/products", to: "products#index"
 
-get "/products/new", to: "products#new"
-post "/products", to: "products#create"
+  get "/products/new", to: "products#new"
+  post "/products", to: "products#create"
 
-get "/products/:id", to: "products#show"
+  get "/products/:id", to: "products#show"
 
-get "/products/:id/edit", to: "products#edit"
-patch "/products/:id", to: "products#update"
-put "/products/:id", to: "products#update"
+  get "/products/:id/edit", to: "products#edit"
+  patch "/products/:id", to: "products#update"
+  put "/products/:id", to: "products#update"
 
-delete "/products/:id", to: "products#destroy"
-root "products#index"
+  delete "/products/:id", to: "products#destroy"
+  root "products#index"
+
 
 end
